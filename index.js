32b2ed68a79d8bd98ef20343140aa92b84eb4cd1
DAVE4547 <benjaminlovering1@gmail.com>
2020-07-27 17:22:11 +0100

    added description method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-exports.message = function(options) {
+exports.title = function(options) {
 
     const {  MessageEmbed } = require('discord.js');
 
@@ -10,6 +10,22 @@ exports.message = function(options) {
 
     const embeded = new MessageEmbed()
         .setTitle(options.content)
+        .setColor(color)
+    return embeded
+}
+
+exports.message = function(options) {
+
+    const {  MessageEmbed } = require('discord.js');
+
+    if ( options.color = null ) {
+        color = '2C2F33'
+    } else {
+        color = options.color
+    }
+
+    const embeded = new MessageEmbed()
+        .setDescription(options.content)
         .setColor(color)
     return embeded
 }
