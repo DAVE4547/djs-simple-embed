ae1d2668c171bf65c1f4ef570ab0cda31f1f99e3
DAVE4547 <benjaminlovering1@gmail.com>
2020-07-27 17:16:02 +0100

    reverted last version

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ exports.message = function(options) {
     const {  MessageEmbed } = require('discord.js');
 
     if ( options.color = null ) {
-        color = '2C2F33',
-        content = options
+        color = '2C2F33'
     } else {
         color = options.color
-        content = options.content
     }
 
     const embeded = new MessageEmbed()
