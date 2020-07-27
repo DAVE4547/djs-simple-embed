b0fa493ab053f81f95d8f135539cb90153db99c0
DAVE4547 <benjaminlovering1@gmail.com>
2020-07-27 16:57:47 +0100

    added more methods

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ exports.message = function(options) {
 
     const {  MessageEmbed } = require('discord.js');
 
+    if ( options.color = null ) {
+        color = '2C2F33'
+    } else {
+        color = options.color
+    }
+
     const embeded = new MessageEmbed()
         .setTitle(options.content)
-        .setColor(options.color)
-
+        .setDescription(options.description)
+        .setColor(color)
+        .setFooter(options.footer)
     return embeded
 }
